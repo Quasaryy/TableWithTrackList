0f76b4a398c4f768c032ab8ef4486654219e3cb0
Yury <quasaryy@gmail.com>
2023-01-24 20:00:04 +0400

    Initial Commit

---
+++ TableWithTrackList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableWithTrackList
+//
+//  Created by Yury on 24.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
